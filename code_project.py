ef117f4991ed08472792e0e1f452939de0d41a43
Loubna Ben Allal <loubna.ben-allal8@etu.univ-lorraine.fr>
2020-11-21 13:08:06 +0100

    Data
    importing data, exploration of the data and visualization of the traffic volume per day( for some months )for each year(2017,2018,2019)

--- code_project.py
+++ code_project.py
@@ -10,6 +10,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
+import os
 
 
 import torch
@@ -19,3 +20,74 @@ from torch.autograd import Variable
 import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import Dataset, DataLoader
+
+data = pd.read_csv('C:\\Users\\33758\\Desktop\\3A Mines\\Machine learning\\Radar Traffic\\Radar_Traffic_Counts.csv')
+
+#Visualization of the data
+data.head()
+data['location_name'].head()
+data[[ 'location_latitude', 'location_longitude','Year','Month', 'Day',
+      'Day of Week']].head(10)
+data[['Hour', 'Minute', 'Time Bin','Direction', 'Volume']].head()
+#shape
+data.shape  #(4603861, 12)
+#columns
+data.columns
+#info
+data.info()
+
+############Data Preprocessing############
+
+#checking missing values
+data.isnull().sum()
+#No missing values in this dataset
+
+#summary of the data
+description=data.describe()
+description.iloc[:,0:2]
+description.iloc[:,2:5]
+description.iloc[:,5:9]
+
+#we can see that we have data over the years: 2017, 2018, 2019
+#Visualization of the data
+#data of the year 2017
+data17=data[data['Year']==2017]
+data17.shape[0]  #801 631
+data17['Month'].unique()  #[12, 11, 10,  9,  8,  7,  6]
+sorted(list(data17['Month'].unique())) #[6, 7, 8, 9, 10, 11, 12]
+#for month 6
+data17_M1=data17[data17['Month']==6]
+data17_M1.shape[0] #3928
+data17_M1['Day of Week'].unique() 
+#volume per day of week fo the year 2017
+plt.plot(data17_M1['Day of Week'],data17_M1['Volume'])
+
+
+#year 2018
+data18=data[data['Year']==2018]
+data18.shape[0]  #2 097 896
+data18['Month'].unique() #[ 1,  8,  2,  9,  3, 10,  4,  5, 11, 12,  6,  7]
+sorted(list(data18['Month'].unique())) #[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+#for month 1
+data18_M1=data18[data18['Month']==1]
+data18_M1.shape[0]
+#volume per month fo the year 2017
+plt.plot(data18['Month'],data18['Volume'])
+
+#year 2019
+data19=data[data['Year']==2019]
+data19.shape[0]  #1 704 334
+data19['Month'].unique() #[ 9,  4,  1,  2,  3,  8,  7,  6,  5, 11, 10]
+sorted(list(data19['Month'].unique())) #[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+#for month 1
+data19_M1=data19[data19['Month']==1]
+data19_M1.shape[0]
+#volume per month fo the year 2017
+plt.plot(data19['Month'],data19['Volume'])
+
+
+#year 2019
+
+
+##Scaling item_cnt_day 
+standard_X=preprocessing.StandardScaler()
