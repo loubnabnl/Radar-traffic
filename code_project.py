c60ef78107379735d977a9abd404d113ec6544d8
Loubna Ben Allal <loubna.ben-allal8@etu.univ-lorraine.fr>
2020-12-10 18:20:52 +0100

    added the teacher's code for the project sales with some modifications
    I tried to apply the the teacher's code for sales prediction withCNNs to this problem, the goal is to predict sales per hour for a week

--- code_project.py
+++ code_project.py
@@ -155,7 +155,9 @@ direct='NB'
 Get_Time_Series(loc,direct)
 
 def Get_Time_Series(location,direction):
+    #location and direction are strings
     extract=data2.loc[data2.location_name==location][data2.Direction==direction]
+    #dates=extract['Date']
     volume=extract['Volume'].to_numpy()
     return volume
 #list of location_names
@@ -163,6 +165,93 @@ names=data2['location_name'].unique().tolist()
 #list of directions 
 directions=['NB','SB','EB','WB','None']
 
-##Standard scaler
-#standard_X=preprocessing.StandardScaler()
-data.head()
+
+###########################################
+## the teacher's code
+###########################################
+
+##convolutional neural network
+class TimeCNN(nn.Module):
+    def __init__(self):
+        super(TimeCNN, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv1d(in_channels=1, out_channels=32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=2, stride=2)
+        )
+        self.layer2 = nn.Sequential(
+            nn.Conv1d(in_channels=32, out_channels=64, kernel_size=3),
+            nn.ReLU(),
+            nn.AdaptiveMaxPool1d(8)
+        )
+        self.fc1 = nn.Linear(in_features=64*8, out_features=128)
+        self.drop = nn.Dropout2d(0.25)
+        self.fc2 = nn.Linear(in_features=128, out_features=32)
+        self.fc3 = nn.Linear(in_features=32, out_features=24*7)
+ 
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc1(out)
+        out = self.drop(out)
+        out = self.fc2(out)
+        out = self.fc3(out)
+        return out
+    
+xmin, xmax = 100.0, -100.0
+vnorm = 1000.0
+minlen = 24*3*30
+# if <8 then layer1 outputs L=7/2=3 which fails because layer2 needs L>=4
+#testing on an example
+si2X, si2Y = {}, {}
+dsi2X, dsi2Y = {}, {}
+#for s,i in dsi2c.keys():
+#    seq = dsi2c[(s,i)]
+seq=Get_Time_Series(names[0], directions[0])
+if len(seq)<=minlen: continue
+xlist, ylist = [], []
+for m in range(minlen, len(seq)-24*7):
+    xx = [seq[z][1]/vnorm for z in range(m)]
+    if max(xx)>xmax: xmax=max(xx)
+    if min(xx)<xmin: xmin=min(xx)
+    xlist.append(torch.tensor(xx,dtype=torch.float32))
+    yy = [seq[m+k][1]/vnorm for k in range(24*7)]
+    ylist.append(torch.tensor(yy,dtype=torch.float32))
+si2X= xlist
+si2Y= ylist
+if True: # build evaluation dataset
+    xx = [seq[z][1]/vnorm for z in range(len(seq)-24*7)]
+    dsi2X = [torch.tensor(xx,dtype=torch.float32)]
+    yy = [seq[len(seq)-24*7+i][1]/vnorm for i in range(24*7)]
+    dsi2Y= [torch.tensor(yy,dtype=torch.float32)]
+
+print("ntrain %d %f %f" % (len(si2X),xmin,xmax))
+
+mod = TimeCNN()
+loss = torch.nn.MSELoss()
+opt = torch.optim.Adam(mod.parameters(),lr=0.01)
+
+xlist = si2X
+if len(xlist)<10: continue
+ylist = si2Y
+idxtr = list(range(len(xlist)))
+for ep in range(5):
+    shuffle(idxtr)
+    lotot=0.
+    mod.train()
+    for j in idxtr:
+        opt.zero_grad()
+        haty = mod(xlist[j].view(1,1,-1))
+        # print("pred %f" % (haty.item()*vnorm))
+        lo = loss(haty,ylist[j].view(1,-1))
+        lotot += lo.item()
+        lo.backward()
+        opt.step()
+
+    # the MSE here is computed on a single sample: so it's highly variable !
+    # to make sense of it, you should average it over at least 1000 (s,i) points
+    mod.eval()
+    haty = mod(dsi2X[(s,i)][0].view(1,1,-1))
+    lo = loss(haty,dsi2Y[(s,i)][0].view(1,-1))
+    print("epoch %d loss %1.9f testMSE %1.9f" % (ep, lotot, lo.item()))
