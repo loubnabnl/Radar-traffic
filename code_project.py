820531b0b8beb9870367407598683132381b5478
Loubna Ben Allal <loubna.ben-allal8@etu.univ-lorraine.fr>
2020-12-12 17:19:07 +0100

    placing code of previous preprocessing at the end

--- code_project.py
+++ code_project.py
@@ -20,78 +20,7 @@ from torch.autograd import Variable
 import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import Dataset, DataLoader
-"""
-data = pd.read_csv('C:\\Users\\33758\\Desktop\\3A Mines\\Machine learning\\Radar Traffic\\Radar_Traffic_Counts.csv')
 
-#Visualization of the data
-data.head()
-data['location_name'].head()
-data[[ 'location_latitude', 'location_longitude','Year','Month', 'Day',
-      'Day of Week']].head(10)
-data[['Hour', 'Minute', 'Time Bin','Direction', 'Volume']].head()
-#shape
-data.shape  #(4603861, 12)
-#columns
-data.columns
-#info
-data.info()
-
-############Data Preprocessing############
-
-#checking missing values
-data.isnull().sum()
-#No missing values in this dataset
-
-#summary of the data
-description=data.describe()
-description.iloc[:,0:2]
-description.iloc[:,2:5]
-description.iloc[:,5:9]
-
-#we can see that we have data over the years: 2017, 2018, 2019
-#month 1:12
-#day 1:31
-#day of week 0:6
-#Hour 0:23
-
-#Visualization of the data
-#data of the year 2017
-data17=data[data['Year']==2017]
-data17.shape[0]  #801 631
-data17['Month'].unique()  #[12, 11, 10,  9,  8,  7,  6]
-sorted(list(data17['Month'].unique())) #[6, 7, 8, 9, 10, 11, 12]
-#for month 6
-data17_M1=data17[data17['Month']==6]
-data17_M1.shape[0] #3928
-data17_M1['Day of Week'].unique() #[4, 3, 2, 1, 0]
-#volume per day of week fo the year 2017
-plt.plot(data17_M1['Day of Week'],data17_M1['Volume'])
-
-
-#year 2018
-data18=data[data['Year']==2018]
-data18.shape[0]  #2 097 896
-data18['Month'].unique() #[ 1,  8,  2,  9,  3, 10,  4,  5, 11, 12,  6,  7]
-sorted(list(data18['Month'].unique())) #[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-#for month 1
-data18_M1=data18[data18['Month']==1]
-data18_M1.shape[0] #162188
-data18_M1['Day of Week'].unique() #[2, 3, 4, 5, 6, 0, 1]
-#volume per month fo the year 2017
-plt.plot(data18_M1['Day of Week'],data18_M1['Volume'])
-
-#year 2019
-data19=data[data['Year']==2019]
-data19.shape[0]  #1 704 334
-data19['Month'].unique() #[ 9,  4,  1,  2,  3,  8,  7,  6,  5, 11, 10]
-sorted(list(data19['Month'].unique())) #[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-#for month 1
-data19_M1=data19[data19['Month']==1]
-data19_M1.shape[0] #135593
-data19_M1['Day of Week'].unique() #[3, 4, 2, 5, 0, 1, 6]
-#volume per month fo the year 2017
-plt.plot(data19_M1['Day of Week'],data19_M1['Volume'])
-"""
 
 ###########################################
 ##DATA PREPROCESSING
@@ -329,6 +258,80 @@ epoch 7 loss 8.773637764 testMSE 0.240398064
 epoch 8 loss 8.925511762 testMSE 0.165904075
 epoch 9 loss 8.683578223 testMSE 0.162461430
     """
+#previous preprocessing
+    
+"""
+data = pd.read_csv('C:\\Users\\33758\\Desktop\\3A Mines\\Machine learning\\Radar Traffic\\Radar_Traffic_Counts.csv')
+
+#Visualization of the data
+data.head()
+data['location_name'].head()
+data[[ 'location_latitude', 'location_longitude','Year','Month', 'Day',
+      'Day of Week']].head(10)
+data[['Hour', 'Minute', 'Time Bin','Direction', 'Volume']].head()
+#shape
+data.shape  #(4603861, 12)
+#columns
+data.columns
+#info
+data.info()
+
+############Data Preprocessing############
+
+#checking missing values
+data.isnull().sum()
+#No missing values in this dataset
+
+#summary of the data
+description=data.describe()
+description.iloc[:,0:2]
+description.iloc[:,2:5]
+description.iloc[:,5:9]
+
+#we can see that we have data over the years: 2017, 2018, 2019
+#month 1:12
+#day 1:31
+#day of week 0:6
+#Hour 0:23
+
+#Visualization of the data
+#data of the year 2017
+data17=data[data['Year']==2017]
+data17.shape[0]  #801 631
+data17['Month'].unique()  #[12, 11, 10,  9,  8,  7,  6]
+sorted(list(data17['Month'].unique())) #[6, 7, 8, 9, 10, 11, 12]
+#for month 6
+data17_M1=data17[data17['Month']==6]
+data17_M1.shape[0] #3928
+data17_M1['Day of Week'].unique() #[4, 3, 2, 1, 0]
+#volume per day of week fo the year 2017
+plt.plot(data17_M1['Day of Week'],data17_M1['Volume'])
+
+
+#year 2018
+data18=data[data['Year']==2018]
+data18.shape[0]  #2 097 896
+data18['Month'].unique() #[ 1,  8,  2,  9,  3, 10,  4,  5, 11, 12,  6,  7]
+sorted(list(data18['Month'].unique())) #[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+#for month 1
+data18_M1=data18[data18['Month']==1]
+data18_M1.shape[0] #162188
+data18_M1['Day of Week'].unique() #[2, 3, 4, 5, 6, 0, 1]
+#volume per month fo the year 2017
+plt.plot(data18_M1['Day of Week'],data18_M1['Volume'])
+
+#year 2019
+data19=data[data['Year']==2019]
+data19.shape[0]  #1 704 334
+data19['Month'].unique() #[ 9,  4,  1,  2,  3,  8,  7,  6,  5, 11, 10]
+sorted(list(data19['Month'].unique())) #[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+#for month 1
+data19_M1=data19[data19['Month']==1]
+data19_M1.shape[0] #135593
+data19_M1['Day of Week'].unique() #[3, 4, 2, 5, 0, 1, 6]
+#volume per month fo the year 2017
+plt.plot(data19_M1['Day of Week'],data19_M1['Volume'])
+"""
    ###########################################
 ## the teacher's code
 ###########################################
